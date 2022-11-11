5b743facd8d58a32b93cc1f5d31c6431e182259d
Noel Bastubbe <noel.bastubbe@gmail.com>
2022-11-11 10:05:30 +0100

    Third try for issue 3

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,14 +2,9 @@ class AuthorsController < ApplicationController
   def new
   end
   def create
-    @author = Author.new(author_params)
+    @author = Author.new(params.require(:author).permit(:first_name), params.require(:author).permit(:last_name), params.require(:author).permit(:homepage))
 
     @author.save
     redirect_to @author
-  end
-
-  private
-  def author_params
-    params.require(:author).permit(:first_name, :last_name, :homepage)
   end
 end
