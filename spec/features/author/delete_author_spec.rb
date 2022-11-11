f892359b9a2967eae0893bc5bb74d55dd2e8ed95
Noel Bastubbe <noel.bastubbe@gmail.com>
2022-11-11 12:42:22 +0100

    First try for issue 9

--- spec/features/author/delete_author_spec.rb
+++ spec/features/author/delete_author_spec.rb
@@ -1,9 +1,9 @@
-describe "Delete author", type: :feature do
-  it "should decrease the author count" do
+describe "Delete Author Action",type: :feature do
+  it "should decrease the Author count" do
     @alan = FactoryBot.create(:author)
-    @oldAuthorCount = Author.count
-    visit "/authors"
-    click_on('Delete')
-    expect(@oldAuthorCount).to eq(Author.count - 1)
+    visit authors_path
+    @count = Author.count
+    find_link('Delete', href: author_path(@alan)).click
+    expect(Author.count).to eq(@count - 1)
   end
 end
