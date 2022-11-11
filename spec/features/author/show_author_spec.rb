6ef6394dad3b5911c330fe9daf97a405e78b2e3e
Noel Bastubbe <noel.bastubbe@gmail.com>
2022-11-11 10:49:44 +0100

    Failing test for issue 4

---
+++ spec/features/author/show_author_spec.rb
@@ -0,0 +1,9 @@
+describe "Show author page", type: :feature do
+  it "should display the name and homepage of the author" do
+    @alan = FactoryBot.create(:author)
+    visit author_path(@alan)
+    expect(page).to have_text("Alan")
+    expect(page).to have_text("Turing")
+    expect(page).to have_text("http://wikipedia.de/Alan_Turing")
+  end
+end
